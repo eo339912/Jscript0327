f198b5c22f312e6cc8b05ea78c2c312ac8d952ce
eo339912 <eo339912@example.com>
2020-03-27 17:53:29 +0900

    0327

--- jScript/src/com/yedam/board/GetBoardListServlet.java
+++ jScript/src/com/yedam/board/GetBoardListServlet.java
@@ -12,47 +12,46 @@ import javax.servlet.http.HttpServletResponse;
 import net.sf.json.JSONArray;
 import net.sf.json.JSONObject;
 
-
-
 /**
  * Servlet implementation class GetBoardListServlet
  */
 @WebServlet("/GetBoardListServlet")
 public class GetBoardListServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public GetBoardListServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#HttpServlet()
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	public GetBoardListServlet() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		//response.getWriter().append("Served at: ").append(request.getContextPath());
-		
+		// response.getWriter().append("Served at: ").append(request.getContextPath());
+
 		JSONObject obj = new JSONObject();
 		JSONArray ary = new JSONArray();
-		
+
 		BoardDAO dao = new BoardDAO();
-		for(Board d : dao.getBoardList()) {
-			obj.put("bNo", d.getbNo());
-			obj.put("bWriter", d.getbWriter());
-			obj.put("bContent", d.getbContent());
-			obj.put("bDate", d.getbDate());
+		for (Board d : dao.getBoardList()) {
+			obj.put("board_no", d.getbNo());
+			obj.put("writer", d.getbWriter());
+			obj.put("content", d.getbContent());
+			obj.put("board_date", d.getbDate());
 			ary.add(obj);
 		}
 		PrintWriter out = response.getWriter();
 		out.print(ary.toString());
-		
+
 		/*
-		 * BoardDAO dao = new BoardDAO();
-		 * List<Board> bdList = dao.getBoardList();		
+		 * BoardDAO dao = new BoardDAO(); List<Board> bdList = dao.getBoardList();
 		 * String bdJson = "[\r\n"; int rCnt = 0; int totalCnt = bdList.size();
 		 * for(Board d : bdList) { bdJson += "{\"bNo\":"+d.getbNo()
 		 * +",\"bWriter\":\""+d.getbWriter() +",\"bContent\":\""+d.getbContent()
@@ -61,17 +60,17 @@ public class GetBoardListServlet extends HttpServlet {
 		 * 
 		 * bdJson += "]";
 		 * 
-		 * PrintWriter out = response.getWriter();
-		 * out.print(bdJson.toString());
+		 * PrintWriter out = response.getWriter(); out.print(bdJson.toString());
 		 */
-		
-		
+
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		doGet(request, response);
 	}
